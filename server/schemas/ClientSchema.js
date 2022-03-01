2c825295a67a5af8a3cb1149e9c2561a48ab976f
Maxine Mason <maxmason2000@gmail.com>
2022-03-01 16:42:19 -0500

    Email option for client schema.

--- server/schemas/ClientSchema.js
+++ server/schemas/ClientSchema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const client = mongoose.Schema({
   companyName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String },
 });
 
 const ClientSchema = mongoose.model("client", client);
